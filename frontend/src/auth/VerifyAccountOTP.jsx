2527ce987f66c05e23b631edb225b06dd1bbcfb8
sharmarke nor <161602232+shrm424@users.noreply.github.com>
2025-06-26 21:11:56 +0300

    VerifyAccountOTP.jsx

--- frontend/src/auth/VerifyAccountOTP.jsx
+++ frontend/src/auth/VerifyAccountOTP.jsx
@@ -21,7 +21,7 @@ const VerifyOtpAccount = () => {
         }
 
         try {
-            const res = await fetch("http://localhost:5000/api/auth/verify-otp", {
+            const res = await fetch("https://bp-prediction-backend.onrender.com/api/auth/verify-otp", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, otp }),
